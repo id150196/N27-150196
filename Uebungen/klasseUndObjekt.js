687ab8ebcdf738be1f03b7ff80e0936e70b64f08
id150196 <id150196@students.berufskolleg-borken.de>
2022-02-11 11:00:16 +0100

    11.02.2022

--- Uebungen/klasseUndObjekt.js
+++ Uebungen/klasseUndObjekt.js
@@ -1,3 +1,7 @@
+console.log("Übungen zu Klasse und objekt")
+console.log ("============================")
+
+
 // Übungen zu Klasse und Objekt
 
 // Übung 1
@@ -7,7 +11,10 @@
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
 
+// zu a) das Objekt der realen Welt ist der Spieler
+
 class Spieler {
+    // zu b)
     constructor(){
         this.Name
         this.Position
@@ -17,23 +24,73 @@ class Spieler {
     }
 }
 
+// zu c)
+// Es wird nun ein konkreter Spieler mit konkreten Eigenschaftswerten erzeugt.
+// Dazu wird der konkrete Spieler deklariert (=bekanntgemacht): let spielerMueller
+// In einem zweiten Schritt wird der konkrete spieler instanziniert = new Spieler()
 let spielerMueller = new Spieler ()
+
+// zu d)
+// Es werden konkrete Eigenschaftswerte in den Arbeitsspeicher geschrieben:
 spielerMueller.Name = "Thomas Müller"
 spielerMueller.Nummer = 25
 spielerMueller.Position = "Strümer"
 spielerMueller.Verein = "FCB"
+spielerMueller.Alter = 18
+
+if(spielerMueller.Alter >= 18){
+    spielerMueller.Volljaehrig = true 
+    console.log("Der Spiler " + spielerMueller.Name + " ist volljährig.")
+}
+
+
+console.log(spielerMueller.Name)
+console.log(spielerMueller.Position)
+console.log("Der Spieler " + spielerMueller.Name + " hat die Nummer " + spielerMueller.Nummer + ".")
 
 
 
 
 // Übung 2
-// In einem Schulprogramm sollen Zeugnisse verwaltet werden. 
+// In einem Schulverwaltungsprogramm sollen Zeugnisse verwaltet werden. 
 // a) Identifizieren Sie das Objekt der realen Welt mit seinen relevanten Eigenschaften
 // b) Erstellen Sie die Klassendefinition
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
 
+// zu a) Das "Zeugnis" ist das Objekt der realen Welt
 
+// zu b)
+class Zeugnis{
+    constructor(){
+        this.Gesamtnote
+        this.Fehlstunden
+        this.SchuelerName
+        this.Faecher
+        this.Klasse
+        this.Geburtsdatum
+    }
+}
+
+// zu c)
+let zeugnisPit = new Zeugnis()
+let zeugnisMax = new Zeugnis ()
+
+// zu d)
+zeugnisPit.SchuelerName = "Pit Kiff"
+zeugnisPit.Fehlstunden = 100
+zeugnisPit.Gesamtnote = 1
+
+
+zeugnisMax.SchuelerName = "Max Muster"
+zeugnisMax.Fehlstunden = 10
+zeugnisMax.Gesamtnote = 2
+
+if(zeugnisMax.Fehlstunden > zeugnisPit.Fehlstunden){
+    console.log ("Max Muster hat mehr Fehlstunden")
+    }else{
+        console.log(" Pit Kiff hat mehr Fehlstunden")
+}
 
 
 
@@ -56,10 +113,6 @@ spielerMueller.Verein = "FCB"
 // b) Erstellen Sie die Klassendefinition
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
-
-
-
-
 
 
 
